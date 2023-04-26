1613774fb776952c70f1759b7f5544b89d178e04
Brian Faust <hello@basecode.sh>
2023-04-26 04:03:27 +0300

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BombenProdukt\BladeIcons\PrimeIcons;
+
+use BombenProdukt\BladeIcons\Facades\IconFamilyRegistry;
+use BombenProdukt\BladeIcons\IconFamily;
+use BombenProdukt\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        IconFamilyRegistry::push(
+            IconFamily::fromDirectory('prime', __DIR__ . '/../resources/svg'),
+        );
+    }
+}
